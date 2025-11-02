7e746c58cb35c4166be7d931ad9017934a601419
Joshua Padilla <markjoshuapadilla.it@gmail.com>
2025-11-02 05:11:15 +0000

    add assign office function

--- src/general/MainFrame.java
+++ src/general/MainFrame.java
@@ -385,6 +385,9 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
+        Head head = new Head();
+        head.setVisible(true);
+        this.dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
